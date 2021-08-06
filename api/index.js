6cef8d6eaa2965cc2f4dd3366fba950d75538b83
&Rewards <88257984+andrewards@users.noreply.github.com>
2021-08-06 16:41:14 -0300

    Instalação e pré-requisitos

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+app.get('/teste', (req, res) => {
+    res.status(200).send({
+        message: 'Boas-vindas à API!',
+    });
+});
+
+app.listen(3000, () => console.log(`Servidor rodando na porta ${port}...`));
+
+module.exports = app;
