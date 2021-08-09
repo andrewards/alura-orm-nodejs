df3e9702731e76e66acda13e79a8a617c47bd78a
&Rewards <88257984+andrewards@users.noreply.github.com>
2021-08-09 03:25:35 -0300

    Criando mais tabelas

---
+++ api/models/matriculas.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class matriculas extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  matriculas.init({
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'matriculas',
+  });
+  return matriculas;
+};
