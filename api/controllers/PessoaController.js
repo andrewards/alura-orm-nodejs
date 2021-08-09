76773ec276391d390288e5e39375b697870571f2
&Rewards <88257984+andrewards@users.noreply.github.com>
2021-08-09 03:00:15 -0300

    CRUD: Atualizar e deletar

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -30,10 +30,48 @@ class PessoaController {
             const { id } = req.params;
             const pessoa = await database.pessoas.findOne({
                 where: {
-                    id: Number(id)
-                }
+                    id: Number(id),
+                },
             });
             return res.status(200).json(pessoa);
+        } catch(err) {
+            return res.status(500).json(err.message);
+        }
+    }
+
+    // UPDATE
+
+    static atualizaPessoa = async (req, res) => {
+        try {
+            const { id } = req.params;
+            const novasInfos = req.body;
+            await database.pessoas.update(novasInfos, {
+                where: {
+                    id: Number(id),
+                },
+            });
+            const pessoaAtualizada = await database.pessoas.findOne({
+                where: {
+                    id: Number(id),
+                },
+            });
+            return res.status(200).json(pessoaAtualizada);
+        } catch(err) {
+            return res.status(500).json(err.message);
+        }
+    }
+
+    // DELETE
+
+    static apagaPessoa = async (req, res) => {
+        try {
+            const { id } = req.params;
+            await database.pessoas.destroy({
+                where: {
+                    id: Number(id),
+                },
+            });
+            return res.status(204).end();
         } catch(err) {
             return res.status(500).json(err.message);
         }
--- api/routes/pessoas.js
+++ api/routes/pessoas.js
@@ -10,4 +10,10 @@ router.post('/pessoas', PessoaController.criaPessoa);
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
 
+// UPDATE
+router.patch('/pessoas/:id', PessoaController.atualizaPessoa);
+
+// DELETE
+router.delete('/pessoas/:id', PessoaController.apagaPessoa);
+
 module.exports = router;
